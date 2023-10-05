6cd1e8d424e1b858317cec418b0227558c85c622
1203385078@qq.com <1203385078@qq.com>
2023-10-05 15:31:31 +0800

    适用于DMA方式的FIFO读写
    Signed-off-by: 1203385078@qq.com <1203385078@qq.com>

--- 指针式/data_fifo.c
+++ 指针式/data_fifo.c
@@ -80,16 +80,115 @@ uint16_t data_fifo_get_data(DATA_FIFO_T *fifo, uint8_t *data, uint16_t data_len)
 		// if(DATA_BUF_SIZE < data_len)
 		// {
 		// 	data_len = DATA_BUF_SIZE;
+		// 	len = data_len;
 		// }
 		
 		len = fifo->data[fifo->index_r].len;
 
 		memcpy(data, fifo->data[fifo->index_r].buf, len);
 
-		len = fifo->data[fifo->index_r].len;
-
 		fifo->index_r ++;
 	}
 	
 	return len;
 }
+
+/**
+ * @brief 获取当前可存入数据地址和数据大小
+ * @param fifo : fifo
+		  data_len : 数据大小
+ * @return 数据地址
+ */
+uint8_t *data_fifo_get_curr_write_point_and_len(DATA_FIFO_T *fifo, uint16_t *data_len)
+{
+	uint16_t fifo_empty_num = data_fifo_get_empty_num(fifo);
+	uint8_t *p_data = NULL;
+
+	p_data = fifo->data[fifo->index_w].buf;
+	*data_len = DATA_BUF_SIZE;
+
+	return p_data;
+}
+
+/**
+ * @brief 当前存入数据完成
+ * @param fifo : fifo
+		  data_len : 数据大小
+ * @return len : 实际存入数据大小
+ */
+uint16_t data_fifo_get_curr_write_point_and_len_done(DATA_FIFO_T *fifo, uint16_t data_len)
+{
+	uint16_t len = 0;
+	uint16_t fifo_empty_num = data_fifo_get_empty_num(fifo);
+	
+	if(fifo_empty_num > 0)
+	{
+		if(DATA_BUF_SIZE < data_len)
+		{
+			data_len = DATA_BUF_SIZE;
+		}
+		
+		len = data_len;
+
+		ifo->data[fifo->index_w].len = len;
+
+		fifo->index_w ++;
+	}
+
+	return len;
+}
+
+/**
+ * @brief 获取当前可读出数据地址和数据大小
+ * @param fifo : fifo
+		  data_addr : 
+		  data_len : 数据大小
+ * @return 数据地址 : NULL，无；其他，成功
+ */
+uint8_t *data_fifo_get_curr_read_point_and_len(DATA_FIFO_T *fifo, uint16_t *data_len)
+{
+	uint16_t len = 0;
+	uint16_t fifo_put_num = data_fifo_get_put_num(fifo);
+	uint8_t *p_data = NULL;
+
+	if(fifo_put_num > 0)
+	{
+		len = fifo->data[fifo->index_r].len;
+
+		p_data = fifo->data[fifo->index_r].buf;
+
+		*data_len = len;
+	}
+
+	return p_data;
+}
+
+/**
+ * @brief 当前读出数据完成
+ * @param fifo : fifo
+		  data_len : 数据大小
+ * @return len : 实际存入数据大小
+ */
+uint16_t data_fifo_get_curr_read_point_and_len_done(DATA_FIFO_T *fifo, uint16_t data_len)
+{
+	uint16_t len = 0;
+	uint16_t fifo_put_num = data_fifo_get_put_num(fifo);
+	uint8_t *p_data = NULL;
+
+	if(fifo_put_num > 0)
+	{
+		// 依赖 data_fifo_put_data 函数检查
+		// if(DATA_BUF_SIZE < data_len)
+		// {
+		// 	data_len = DATA_BUF_SIZE;
+		// }
+		
+		len = fifo->data[fifo->index_r].len;
+
+		fifo->index_r ++;
+	}
+
+	return len;
+}
+
+
